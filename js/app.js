81214a5d18c88358f52c9159992cb3041556ab70
agustin <agustinlunalde@gmail.com>
2023-11-08 19:17:47 -0300

    Trabajo final

--- js/app.js
+++ js/app.js
@@ -4,45 +4,66 @@ const modalContainer = document.getElementById("modal-container");
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-casacas.forEach((casaca) => {
-let content = document.createElement("div");
-content.className = "card";
-content.innerHTML = `
-<img src="${casaca.img}">
-<h3 class="nombre">${casaca.nombre}</h3>
-<p class="costo">${casaca.precio}</p>
-`;
-shopContent.append(content);
+const getProduct = async () => {
+    const respuesta = await fetch("data.json");
+    const data = await respuesta.json();
+    console.log(data)
 
-
-let comprar = document.createElement("button")
-comprar.innerText = "comprar";
-content.append(comprar)
-comprar.className = "comprar"; 
-
-
-comprar.addEventListener("click", () => {
-    const repeticion = carrito.some ((repeticionCasaca) => repeticionCasaca.id === casaca.id);
-if(repeticion){
-    carrito.map((prod) => {
-        if(prod.id === casaca.id){
-            prod.cantidad ++;
-        }
-    });
-}else {
-    carrito.push({
-        id: casaca.id,
-        nombre: casaca.nombre,
-        precio: casaca.precio,
-        img: casaca.img,
-        cantidad: casaca.cantidad,
-    });
-    saveLocal();
+    data.forEach((casaca) => {
+        let content = document.createElement("div");
+        content.className = "card";
+        content.innerHTML = `
+        <img src="${casaca.img}">
+        <h3 class="nombre">${casaca.nombre}</h3>
+        <p class="costo">${casaca.precio}</p>
+        `;
+        shopContent.append(content);
+        
+        
+        let comprar = document.createElement("button")
+        comprar.innerText = "comprar";
+        content.append(comprar)
+        comprar.className = "comprar"; 
+        
+        
+        comprar.addEventListener("click", () => {
+            Toastify({
+        
+                text: "Se agrego al carrito",
+                
+                duration: 2000
+                
+                }).showToast();
+        
+            const repeticion = carrito.some ((repeticionCasaca) => repeticionCasaca.id === casaca.id);
+        if(repeticion){
+            carrito.map((prod) => {
+                if(prod.id === casaca.id){
+                    prod.cantidad ++;
+                }
+            });
+        }else {
+            carrito.push({
+                id: casaca.id,
+                nombre: casaca.nombre,
+                precio: casaca.precio,
+                img: casaca.img,
+                cantidad: casaca.cantidad,
+            });
+            saveLocal();
+        }; 
+        });
+        });
+        const saveLocal = () => {
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        };
 }; 
-});
-});
-const saveLocal = () => {
-localStorage.setItem("carrito", JSON.stringify(carrito));
-};
+getProduct();
+
+
+
+
+
+
 
 
